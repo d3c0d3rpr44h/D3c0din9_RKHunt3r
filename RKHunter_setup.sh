3b7885d122991580b874b2e727d2109f70e30767
Decoder Praah <d3c0d3rpr44h@gmail.com>
2024-09-14 23:31:37 +0530

    Add files via upload

---
+++ RKHunter_setup.sh
@@ -0,0 +1,8 @@
+sudo apt install rkhunter
+
+sudo rkhunter --propupd
+
+sudo rm /etc/cron.daily/rkhunter
+
+sudo rm /etc/cron.weekly/rkhunter
+
---
+++ RKHunter_showlog.sh
@@ -0,0 +1,4 @@
+sudo cat /var/log/rkhunter.log
+
+sudo less /var/log/rkhunter.log
+
